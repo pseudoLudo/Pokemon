8b50a8745913fbff9e3ee8c4ba47f894a24af3af
Ruthvik <ruthvik.2123@gmail.com>
2025-04-08 16:09:24 +0530

    replace if statements with switch statement

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -4,35 +4,33 @@ using namespace std;
 int main() {
 
     string player_name;
+    string chosen_pokemon;
     int choice;
 
+    cout << "Welcome to the world of Pokémon! I am Professor Oak.People call me the Pokemon Professor!\n";
+    cout << "Professor Oak: First, tell me, what’s your name?\n";
     cout << "Enter your name" << endl;
     cin >> player_name;
-    cout << "Great Start, " << player_name << " Looks like you’ve mastered the opening of every code adventure. Keep this up, and you’ll be a coding master in no time!" << endl << endl;
+    cout << "Great Start, " << player_name << " Professor Oak: You must be eager to start your adventure. But first, you’ll need a Pokemon of your own!" << endl << endl;
 
-    cout << "Welcome to the world of Pokémon! I am Professor Oak.\n";
+
     cout << "You can choose one of the following Pokémon:\n";
     cout << "1. Bulbasaur\n2. Charmander\n3. Squirtle\n";
     cout << "Which Pokémon would you like to choose? Enter the number: ";
 
     cin >> choice;
-    if (choice == 1)
+
+    switch (choice)
     {
-        cout << "You chose Bulbasaur! A wise choice.\n";
-    }
-    else if (choice == 2)
-    {
-        cout << "You chose Charmander! A fiery choice.\n";
-    }
-    else if (choice == 3)
-    {
-        cout << "You chose Squirtle! A cool choice.\n";
-    }
-    else
-    {
-        cout << "Invalid choice. Please restart the game.\n";
+        case 1: cout << "You chose Bulbasaur! A wise choice." << endl;chosen_pokemon = "Bulbasaur";break;
+        case 2: cout << "You chose Charmander! A fiery choice." << endl;chosen_pokemon = "Charmander";break;
+        case 3: cout << "You chose Squirtle! A cool choice." << endl;chosen_pokemon = "Squirtle";break;
+        default: cout << "Hmm, that doesn't seem right, how about you with my personal favorite, Pikachu!"<< endl;chosen_pokemon = "Pikachu";break;
     }
 
+    cout << "" << endl;
+    cout << "Professor Oak: " << chosen_pokemon << " and you, "<< player_name << ", are going to be the best of friends!\n";
+    cout << "Professor Oak: Your journey begins now! Get ready to explore the vast world of Pokemon!\n"<<endl;
     return 0;
 
 }
